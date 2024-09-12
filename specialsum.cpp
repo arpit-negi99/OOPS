fd96c90f567838337cecde39e0c487d42058e1b6
arpit-negi99 <negiarpit2005@gmail.com>
2024-09-12 07:56:35 +0100

    specialsum.cpp

--- specialsum.cpp
+++ specialsum.cpp
@@ -23,3 +23,39 @@ Typical input:                      Typical output:
 10                                  9
 2 1 3 9 2 4 -10 -9 1 3 
 */
+#include <iostream>
+using namespace std;
+int main()
+{
+ int n , c = 1;
+ cout << "Enter the size of the array - ";
+ cin >> n;
+ int a[n];
+ int currentSum = 0;
+ int maxSum = 0;
+ int maxSumIndex = 0;
+ cout << "Enter the array elements - "<<endl;
+ for (int i = 0; i < n; i++)
+ {
+ cin >> a[i];
+ }
+ for (int i = 0; i < n; i++)
+ {
+ for (int j = i; n - j >= c; j += c - 1)
+ {
+ for (int k = j; k < j + c; k++)
+ {
+ currentSum += a[k];
+ }
+ c++;
+ }
+ c = 1;
+ if (currentSum > maxSum)
+ {
+ maxSum = currentSum;
+ maxSumIndex = i;
+ }
+ currentSum = 0;
+ }
+ cout <<"The Maximum Sum is -"<< "\n" <<maxSum <<endl;
+}
