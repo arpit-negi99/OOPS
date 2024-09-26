0c2edc65f6397cac1bfb94b8a6816c339d84bf4d
arpit-negi99 <negiarpit2005@gmail.com>
2024-09-26 08:41:56 +0100

    Update twelve.cpp

--- twelve.cpp
+++ twelve.cpp
@@ -1,4 +1,12 @@
+/*
+Create a class called Time that has separate int member data for hours, minutes and seconds.
+One constructor should initialize this data to 0, and another should initialize it to
+fixed values. A member function should display it in 11:59:59 format.
+A member function named add() should add two objects of type time passed as arguments.
+A main ( ) program should create two initialized values together, leaving the result
+in the third time variable. Finally it should display the value of this third variable.
 #include<iostream>
+*/
 using namespace std;
 class time{
     private:
